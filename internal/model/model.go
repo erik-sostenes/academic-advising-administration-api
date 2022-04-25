95d3ce1a0d7e8e53352619ddb238fc45631dd441
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-04-25 14:58:10 -0500

    MockTeacherSchedule  represents the mock of a teacher schedule.
    1. Add insertTeacherSchedules sql query to select teacher schedules by
       Teacher Tuition.
    
    issue #3

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-// Schedule represents a teacher schedule 
+// Schedule represents a schedule 
 type Schedule struct {
 	ScheduleId 	  string    `json:"schedule_id"`
 	ScheduleAt 	  time.Time `json:"schedule_at"`
@@ -12,4 +12,12 @@ type Schedule struct {
 	ToDate 		  time.Time `json:"to_date"`
 	StudentAccountant uint8     `json:"student_accountant"`
 	TeacherTuition 	  string    `json:"teacher_tuition"`
+}
+
+// TeacherSchedule represents a teacher schedule
+type TeacherSchedule struct {
+	Day            string   `json:"day"` 
+	TeacherName    string   `json:"teacher_name"`
+	SurnameTeacher string   `json:"surname"`
+	Schedule       Schedule `json:"schedule"`
 }
