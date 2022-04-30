a36d1a503a06dc6b7456e31e520ecfd8d32bcbd5
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-04-30 02:08:52 -0500

    Start runnig the program.
    issue #5

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
+	"github.com/itsoeh/academy-advising-administration-api/internal/dependency"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	// start running the program
+	if err := dependency.Run(); err != nil {
+		log.Fatal(err)
+	}
 } 
