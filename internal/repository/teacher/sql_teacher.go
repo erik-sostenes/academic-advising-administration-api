2a8691be96a03935f86b544328fd59729c5507ee
erik-sostenes-simon <eriksostenessimon@gmail.com>
2022-06-29 18:34:07 -0500

    Refactor code, remove the sql queries related to students.
    issue #15

--- internal/repository/teacher/sql_teacher.go
+++ internal/repository/teacher/sql_teacher.go
@@ -24,39 +24,4 @@ var (
 									INNER JOIN coordinators co ON(co.tuition = su.coordinator_tuition)
 		WHERE s.subject_id = ? AND u.university_course_id = ? AND t.is_active = true;
 		`
-		selectStudentRequest = `
-			SELECT
-				st.tuition,
-				st.name,
-				st.email,
-  			c.cubicle,
-				s.subject,
-				u.building_number,
-				a.advisory_id,
-				ts.teacher_schedule_id
-			FROM advisories a
-				INNER JOIN students st ON(st.tuition = a.student_tuition)
-					INNER JOIN subjects s ON(s.subject_id = a.subject_id)
-						INNER JOIN teachers t ON(t.tuition = a.teachers_tuition)
-							INNER JOIN university_courses u ON(u.university_course_id = a.university_course_id)
-								INNER JOIN cubicles c ON(c.cubicle_id = t.cubicle_id)
-									INNER JOIN academic_advisory_schedule_record aasr ON(aasr.advisory_id = a.advisory_id)
-										INNER JOIN teachers_schedules ts ON(ts.teacher_schedule_id = aasr.teacher_schedule_id)
-			WHERE t.is_active = true AND a.is_accepted = false AND t.tuition = ?;`
-
-			selectStudentRequestAccepted = `
-				SELECT
-					st.tuition,
-					st.name,
-					st.email,
-					c.cubicle,
-					s.subject,
-					u.university_course
-				FROM advisories a
-  				INNER JOIN students st ON(st.tuition = a.student_tuition)
-  					INNER JOIN subjects s ON(s.subject_id = a.subject_id)
-  						INNER JOIN teachers t ON(t.tuition = a.teachers_tuition)
-  							INNER JOIN university_courses u ON(u.university_course_id = a.university_course_id)
-  								INNER JOIN cubicles c ON(c.cubicle_id = t.cubicle_id)
-				WHERE t.is_active = true AND a.is_accepted = true AND t.tuition = ?`
-) 
+	) 
